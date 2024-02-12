7b007f2349b8925c7d3d2fb7032b5a1f5ad0e3a8
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-02-12 17:18:56 +0530

    list rendering

--- base/views.py
+++ base/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+rooms = [
+    {'id':1, 'name':"lets learn the python"},
+    {'id':2, 'name':"lets learn the angular"},
+    {'id':3, 'name':"lets learn the react"},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'rooms':rooms})
 def room(request):
     return render(request, 'room.html')
